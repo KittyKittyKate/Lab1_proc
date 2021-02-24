4c53b83d0996e9bf1ce87b644c83639a2bc7fa8a
KittyKittyKate <ivasam248@gmail.com>
2021-02-24 13:45:19 +0700

    Добавлен новый класс - "Загадки"

---
+++ puzzle.h
@@ -0,0 +1,13 @@
+#ifndef __puzzle__
+#define __puzzle__
+#include <iostream>
+
+namespace collection_of_wisdom {
+	// загадка
+	struct puzzle
+	{
+		char puzzle_str[100];
+		char answer[20];
+	};
+} // end collection_of_wisdom namespace
+#endif
